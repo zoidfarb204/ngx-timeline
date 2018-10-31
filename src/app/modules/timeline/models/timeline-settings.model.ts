b89c344a7e526a5eb44a2da48b9a72bb9dc7b8e5
zoidfarb204 <laxin204@gmail.com>
2018-10-30 19:47:15 -0500

    Add labels
    Added labels and the ability to turn on and off labels.

--- src/app/modules/timeline/models/timeline-settings.model.ts
+++ src/app/modules/timeline/models/timeline-settings.model.ts
@@ -1,4 +1,6 @@
 export class TimelineSettingsModel {
     inactiveColor: String = 'lightgray';
     activeColor: String = 'lightblue';
+    showLabel = false;
+    labelColor: String = 'lightblue';
 }
